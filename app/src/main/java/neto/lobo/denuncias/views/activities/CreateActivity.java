35302dfdb18a89abb892ce9086e0b43f957e085d
Unknown <itagores.lira@ufersa.edu.br>
2018-07-23 19:48:50 -0300

    teste

--- app/src/main/java/neto/lobo/denuncias/views/activities/CreateActivity.java
+++ app/src/main/java/neto/lobo/denuncias/views/activities/CreateActivity.java
@@ -11,5 +11,6 @@ public class CreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+	//teste
     }
 }
